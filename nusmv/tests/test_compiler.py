8613b93376f6eff50864d927a93275f8d021e99a
Russel <emkil.russel@gmail.com>
2017-07-17 01:35:19 +0300

    [fix] test_compile has been rewritten with regular expressions

--- nusmv/tests/test_compiler.py
+++ nusmv/tests/test_compiler.py
@@ -2,6 +2,7 @@ import unittest
 from nusmv.compiler import *
 from nusmv.parser import *
 from nusmv.generator import *
+import re
 
 STUB_WORLD_MAPPINGS = {K_WORLD_MAX_X: 100, K_WORLD_MAX_Y: 100}
 STUB_ISLAND_MAPPINGS = {K_ISLAND_CENTER_X: 50, K_ISLAND_CENTER_Y: 50, K_ISLAND_SMALL_RADIUS: 25,
@@ -42,40 +43,26 @@ class TestCompiler(unittest.TestCase):
         specification = Specification(STUB_WORLD_MAPPINGS, STUB_ISLAND_MAPPINGS, penguin_mappings,
                                       snowball_mappings)
 
-        template_code = """MODULE Snowball\n  DEFINE\nPos = @flew@;\n  VAR\n  a: 0..10;
+        template_code = """MODULE Snowball\n  DEFINE\nPos = @flew@;\n  VAR\n  a: 0..10;\n\
 MODULE Penguin\n  DEFINE\nPos = @moved@;\n VAR\n  b: 0..1;\n"""
 
         template = parse_template_from_string(template_code)
         compiler = Compiler(template, specification, PenguinGenerator(specification), SnowballGenerator(specification))
 
-        result = """MODULE Snowball\n  DEFINE\nPos = case
-  direction in 0..30 & (next(x) - x) = 1 & (next(y) - y) = 0 : TRUE;
-  direction in 31..60 & (next(x) - x) = 1 & (next(y) - y) = 1 : TRUE;
-  direction in 61..120 & (next(x) - x) = 0 & (next(y) - y) = 1 : TRUE;
-  direction in 121..149 & (next(x) - x) = -1 & (next(y) - y) = 1 : TRUE;
-  direction in 150..209 & (next(x) - x) = -1 & (next(y) - y) = 0 : TRUE;
-  direction in 210..240 & (next(x) - x) = -1 & (next(y) - y) = -1 : TRUE;
-  direction in 241..300 & (next(x) - x) = 0 & (next(y) - y) = -1 : TRUE;
-  direction in 301..330 & (next(x) - x) = 1 & (next(y) - y) = -1 : TRUE;
-  direction in 331..359 & (next(x) - x) = 1 & (next(y) - y) = 0 : TRUE;
-  TRUE : FALSE;
-esac;\n  VAR\n  a: 0..10;\n"""
+        case_start = "case\\n"
+        case_expr = """(  direction ((= [0-9]+)|(in [0-9]+\\.\\.[0-9]+)) \
+& \\(next\\(x\\) - x\\) = -?[0-9]+ & \\(next\\(y\\) - y\\) = -?[0-9]+ : TRUE;\\n)*"""
+        case_end = "  TRUE : FALSE;\\nesac"
+        result_expr = case_start + case_expr + case_end
+        result = "MODULE Snowball\\n  DEFINE\\nPos = " + result_expr + ";\\n  VAR\n  a: 0\\.\\.10;\\n"
 
-        result = result + """MODULE Penguin\n  DEFINE\nPos = case
-  next(direction) in 0..30 & (next(x) - x) = 1 & (next(y) - y) = 0 : TRUE;
-  next(direction) in 31..60 & (next(x) - x) = 1 & (next(y) - y) = 1 : TRUE;
-  next(direction) in 61..120 & (next(x) - x) = 0 & (next(y) - y) = 1 : TRUE;
-  next(direction) in 121..149 & (next(x) - x) = -1 & (next(y) - y) = 1 : TRUE;
-  next(direction) in 150..209 & (next(x) - x) = -1 & (next(y) - y) = 0 : TRUE;
-  next(direction) in 210..240 & (next(x) - x) = -1 & (next(y) - y) = -1 : TRUE;
-  next(direction) in 241..300 & (next(x) - x) = 0 & (next(y) - y) = -1 : TRUE;
-  next(direction) in 301..330 & (next(x) - x) = 1 & (next(y) - y) = -1 : TRUE;
-  next(direction) in 331..359 & (next(x) - x) = 1 & (next(y) - y) = 0 : TRUE;
-  TRUE : FALSE;
-esac;\n VAR\n  b: 0..1;\n"""
-
-        assert compiler.model_code == compiler.compile()
-        assert compiler.compile() == result
+        case_start = "case\\n"
+        case_expr = """(  next\\(direction\\) ((= [0-9]+)|(in [0-9]+\\.\\.[0-9]+)) \
+& \\(next\\(x\\) - x\\) = -?[0-9]+ & \\(next\\(y\\) - y\\) = -?[0-9]+ : TRUE;\\n)*"""
+        case_end = "  TRUE : FALSE;\\nesac"
+        result_expr = case_start + case_expr + case_end
+        result = result + "MODULE Penguin\\n  DEFINE\\nPos = " + result_expr + ";\n VAR\n  b: 0\\.\\.1;\\n"
+        self.assertIsNotNone(re.match(result, compiler.compile()))
 
     def test_with_two_tags_and_test_case(self):
         snowball_mappings = {K_SNOWBALL_RADIUS: 10, K_SNOWBALL_FLY_VELOCITY: 1}
@@ -84,47 +71,33 @@ esac;\n VAR\n  b: 0..1;\n"""
         specification = Specification(STUB_WORLD_MAPPINGS, STUB_ISLAND_MAPPINGS, penguin_mappings,
                                       snowball_mappings)
 
-        template_code = """MODULE Snowball\n  DEFINE\nPos = @flew@;\n  VAR\n  a: 0..10;
+        template_code = """MODULE Snowball\n  DEFINE\nPos = @flew@;\n  VAR\n  a: 0..10;\n\
 MODULE Penguin\n  DEFINE\nPos = @moved@;\n VAR\n  b: 0..1;\n"""
 
         template = parse_template_from_string(template_code)
         compiler = Compiler(template, specification, PenguinGenerator(specification), SnowballGenerator(specification))
 
-        test_case_code = """MODULE TestCase\n  VAR\n  p1: Penguin();\n"""
-        test_cases = parse_modules_from_string(test_case_code)
+        test_cases = parse_modules_from_string("""MODULE TestCase\n  VAR\n  p1: Penguin();\n""")
 
-        main_code = """MODULE main\n  VAR\n    test: TestCase;\n"""
+        case_start = "case\\n"
+        case_expr = """(  direction ((= [0-9]+)|(in [0-9]+\\.\\.[0-9]+)) \
+& \\(next\\(x\\) - x\\) = -?[0-9]+ & \\(next\\(y\\) - y\\) = -?[0-9]+ : TRUE;\\n)*"""
+        case_end = "  TRUE : FALSE;\\nesac"
+        result_expr = case_start + case_expr + case_end
+        result = "MODULE Snowball\\n  DEFINE\\nPos = " + result_expr + ";\n  VAR\n  a: 0\\.\\.10;\\n"
 
-        result = """MODULE Snowball\n  DEFINE\nPos = case
-  direction in 0..30 & (next(x) - x) = 1 & (next(y) - y) = 0 : TRUE;
-  direction in 31..60 & (next(x) - x) = 1 & (next(y) - y) = 1 : TRUE;
-  direction in 61..120 & (next(x) - x) = 0 & (next(y) - y) = 1 : TRUE;
-  direction in 121..149 & (next(x) - x) = -1 & (next(y) - y) = 1 : TRUE;
-  direction in 150..209 & (next(x) - x) = -1 & (next(y) - y) = 0 : TRUE;
-  direction in 210..240 & (next(x) - x) = -1 & (next(y) - y) = -1 : TRUE;
-  direction in 241..300 & (next(x) - x) = 0 & (next(y) - y) = -1 : TRUE;
-  direction in 301..330 & (next(x) - x) = 1 & (next(y) - y) = -1 : TRUE;
-  direction in 331..359 & (next(x) - x) = 1 & (next(y) - y) = 0 : TRUE;
-  TRUE : FALSE;
-esac;\n  VAR\n  a: 0..10;\n"""
+        case_start = "case\\n"
+        case_expr = """(  next\\(direction\\) ((= [0-9]+)|(in [0-9]+\\.\\.[0-9]+)) \
+& \\(next\\(x\\) - x\\) = -?[0-9]+ & \\(next\\(y\\) - y\\) = -?[0-9]+ : TRUE;\\n)*"""
+        case_end = "  TRUE : FALSE;\\nesac"
+        result_expr = case_start + case_expr + case_end
+        result = result + "MODULE Penguin\\n  DEFINE\\nPos = " + result_expr + ";\\n VAR\\n  b: 0\\.\\.1;\\n"
 
-        result = result + """MODULE Penguin\n  DEFINE\nPos = case
-  next(direction) in 0..30 & (next(x) - x) = 1 & (next(y) - y) = 0 : TRUE;
-  next(direction) in 31..60 & (next(x) - x) = 1 & (next(y) - y) = 1 : TRUE;
-  next(direction) in 61..120 & (next(x) - x) = 0 & (next(y) - y) = 1 : TRUE;
-  next(direction) in 121..149 & (next(x) - x) = -1 & (next(y) - y) = 1 : TRUE;
-  next(direction) in 150..209 & (next(x) - x) = -1 & (next(y) - y) = 0 : TRUE;
-  next(direction) in 210..240 & (next(x) - x) = -1 & (next(y) - y) = -1 : TRUE;
-  next(direction) in 241..300 & (next(x) - x) = 0 & (next(y) - y) = -1 : TRUE;
-  next(direction) in 301..330 & (next(x) - x) = 1 & (next(y) - y) = -1 : TRUE;
-  next(direction) in 331..359 & (next(x) - x) = 1 & (next(y) - y) = 0 : TRUE;
-  TRUE : FALSE;
-esac;\n VAR\n  b: 0..1;\n"""
+        test_case_expr = "MODULE TestCase\\n  VAR\\n  p1: Penguin\\(\\);\\n""" + "\\n\\n"
+        main_code_expr = "MODULE main\\n  VAR\\n    test: TestCase;\\n"
+        result = result + "\\n" + test_case_expr + main_code_expr
 
-        result = result + "\n" + test_cases[0].code + "\n\n" + main_code
-
-        assert compiler.model_code != compiler.compile(test_cases[0])
-        assert compiler.compile(test_cases[0]) == result
+        self.assertIsNotNone(re.match(result, compiler.compile(test_cases[0])))
 
     def test_with_two_tags_test_case_and_insertion(self):
         snowball_mappings = {K_SNOWBALL_RADIUS: 10, K_SNOWBALL_FLY_VELOCITY: 1}
@@ -139,27 +112,19 @@ MODULE Penguin\n  DEFINE\nPos = @moved@;\n VAR\n  b: 0..1;\n"""
         template = parse_template_from_string(template_code)
         compiler = Compiler(template, specification, PenguinGenerator(specification), SnowballGenerator(specification))
 
-        test_case_code = """MODULE TestCase\n  VAR\n  p1: Penguin();\n"""
-        test_cases = parse_modules_from_string(test_case_code)
+        test_cases = parse_modules_from_string("""MODULE TestCase\n  VAR\n  p1: Penguin();\n""")
 
-        main_code = """MODULE main\n  VAR\n    test: TestCase;\n"""
+        case_start = "case\\n"
+        case_expr = """(  direction ((= [0-9]+)|(in [0-9]+\\.\\.[0-9]+)) \
+& \\(next\\(x\\) - x\\) = -?[0-9]+ & \\(next\\(y\\) - y\\) = -?[0-9]+ : TRUE;\\n)*"""
+        case_end = "  TRUE : FALSE;\\nesac"
+        result_expr = case_start + case_expr + case_end
+        result = "MODULE Snowball\\n  DEFINE\\nPos = " + result_expr + ";\n  VAR\n  a: 0\\.\\.10;\\n"
 
-        result = """MODULE Snowball\n  DEFINE\nPos = case
-  direction in 0..30 & (next(x) - x) = 1 & (next(y) - y) = 0 : TRUE;
-  direction in 31..60 & (next(x) - x) = 1 & (next(y) - y) = 1 : TRUE;
-  direction in 61..120 & (next(x) - x) = 0 & (next(y) - y) = 1 : TRUE;
-  direction in 121..149 & (next(x) - x) = -1 & (next(y) - y) = 1 : TRUE;
-  direction in 150..209 & (next(x) - x) = -1 & (next(y) - y) = 0 : TRUE;
-  direction in 210..240 & (next(x) - x) = -1 & (next(y) - y) = -1 : TRUE;
-  direction in 241..300 & (next(x) - x) = 0 & (next(y) - y) = -1 : TRUE;
-  direction in 301..330 & (next(x) - x) = 1 & (next(y) - y) = -1 : TRUE;
-  direction in 331..359 & (next(x) - x) = 1 & (next(y) - y) = 0 : TRUE;
-  TRUE : FALSE;
-esac;\n  VAR\n  a: 0..10;\n"""
+        result = result + """MODULE Penguin\\n  DEFINE\\nPos = FALSE;\\n VAR\\n  b: 0\\.\\.1;\\n"""
 
-        result = result + """MODULE Penguin\n  DEFINE\nPos = FALSE;\n VAR\n  b: 0..1;\n"""
+        test_case_expr = "MODULE TestCase\\n  VAR\\n  p1: Penguin\\(\\);\\n""" + "\\n\\n"
+        main_code_expr = "MODULE main\\n  VAR\\n    test: TestCase;\\n"
+        result = result + "\\n" + test_case_expr + main_code_expr
 
-        result = result + "\n" + test_cases[0].code + "\n\n" + main_code
-
-        assert compiler.model_code != compiler.compile(test_cases[0])
-        assert compiler.compile(test_cases[0]) == result
+        self.assertIsNotNone(re.match(result, compiler.compile(test_cases[0])))
